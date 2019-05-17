76c1301a71b7e2b7b805322a3d63b5cc4da65544
Anwin Joselyn <anwinj@gmail.com>
2019-05-17 22:09:10 +0530

    Added Stripe keys to config

--- src/config.js
+++ src/config.js
@@ -1,4 +1,5 @@
 const dev = {
+  STRIPE_KEY: "pk_test_D1KI5S92zgYEjuM2jzIY5osV006kVUoIoc",
   s3: {
     REGION: "us-east-2",
     BUCKET: "notes-app-2-api-dev-attachmentsbucket-1b24jis50b4j9"
@@ -16,6 +17,7 @@ const dev = {
 };
 
 const prod = {
+  STRIPE_KEY: "pk_test_D1KI5S92zgYEjuM2jzIY5osV006kVUoIoc",
   s3: {
     REGION: "us-east-2",
     BUCKET: "notes-app-2-api-prod-attachmentsbucket-bbur3x7atf9b"
